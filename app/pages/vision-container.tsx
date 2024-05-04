653856995c6db42e5681303aba78a2ff7d2e536f
davidwale <davidwale2003@gmail.com>
2024-05-04 04:06:47 +0100

    completed vision container

--- app/pages/vision-container.tsx
+++ app/pages/vision-container.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const VisionContainer: React.FC = () => {
     return (
         <div className="flex flex-row">
-            <div className='mt-60 ml-40'>
+            <div className='mt-48 ml-40'>
             <h1 className="text-2xl mb-4 text-white monument-extended">
                     OUR VISION IS TO <br />
                     SUPPORT THE INNOVATION <br />
@@ -15,19 +15,19 @@ const VisionContainer: React.FC = () => {
                 </span>
             </h1>
             </div>
-            <section className='text-white mt-48 ml-36'>
-                <div className='flex flex-row'>
+            <section className='text-white mt-48 ml-44'>
+                <div className='flex flex-row py-10 border-t border-gray-900'>
                     <div>
-                        <img className="h-16 w-16" src="./images/profitability.png" alt="Logo" />
+                        <img className="h-16 w-16 bottom-2 relative" src="./images/profitability.png" alt="Logo" />
                     </div>
                     <div>
                         <span>
-                            <p className='pl-14'>
+                            <p className='pl-6'>
                                 Profitability and <br /> Growth
                             </p>
 
                         </span>
-                        <p className='text-xs mt-8 pl-14'>
+                        <p className='text-xs mt-8 pl-7'>
                             At Creon, we handpick cutting-edge AI projects and <br />
                             offer our community and token holders early access <br />
                             and investment opportunities. Our community <br />
@@ -37,28 +37,43 @@ const VisionContainer: React.FC = () => {
                         </p>
                     </div>
                 </div>
-                <div>
+                <div className='flex flex-row py-10 border-t border-gray-900'>
                     <div>
-                        
+                        <img className="h-16 w-16 bottom-2 relative" src="./images/transparency.png" alt="Logo" />
                     </div>
                     <div>
+                        <span>
+                            <p className='pl-6'>
+                                Transparent & fair Decentralized <br /> Earnings
+                            </p>
 
+                        </span>
                     </div>
                 </div>
-                <div>
+                <div className='flex flex-row py-10 border-t border-b border-gray-900'>
                     <div>
-                        
+                        <img className="h-16 w-16 bottom-2 relative" src="./images/launch.png" alt="Logo" />
                     </div>
                     <div>
+                        <span>
+                            <p className='pl-6'>
+                                Launching the <br /> future
+                            </p>
 
+                        </span>
                     </div>
                 </div>
-                <div>
+                <div className='flex flex-row py-10  border-b border-gray-900'>
                     <div>
-                        
+                        <img className="h-16 w-16 bottom-2 relative" src="./images/limitless.png" alt="Logo" />
                     </div>
                     <div>
+                        <span>
+                            <p className='pl-6'>
+                                Limitless Possibilities of AI <br /> & crypto
+                            </p>
 
+                        </span>
                     </div>
                 </div>
             </section>
