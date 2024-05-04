63afcc6eb4f841565dfa9612286050ec5d43a489
davidwale <davidwale2003@gmail.com>
2024-05-04 14:47:18 +0100

    added functionality

--- app/pages/vision-container.tsx
+++ app/pages/vision-container.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+"use client";
+
+import React, {useState} from 'react';
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from 'react-icons/md';
 
 const VisionContainer: React.FC = () => {
+    const [showText, setShowText] = useState(false);
+    
+    const toggleText = () => {
+        setShowText(!showText);
+    };
+
     return (
         <div className="flex md:flex-row flex-col justify-between">
             <div className='mt-48 w-full'>
@@ -20,7 +28,7 @@ const VisionContainer: React.FC = () => {
                 </span>
             </div>
             <section className='text-white md:mt-48 md:ml-44 w-full'>
-                <div className='flex flex-row py-10 border-t border-gray-900'>
+                <div className='flex flex-row py-10 border-t border-gray-900 hover:cursor-pointer' onClick={toggleText}>
                     <div>
                         <img className="h-16 w-16 bottom-2 relative" src="./images/profitability.png" alt="Logo" />
                     </div>
@@ -29,18 +37,20 @@ const VisionContainer: React.FC = () => {
                             <p className='pl-6 satoshi-bold'>
                                 Profitability and <br /> Growth
                             </p>
-                        <MdOutlineKeyboardArrowDown />
+                       {showText ? <MdOutlineKeyboardArrowUp /> : <MdOutlineKeyboardArrowDown />}
                         </span>
-                        <p className='text-xs mt-8 pl-7 satoshi-regular'>
-                            At Creon, we handpick cutting-edge AI projects and 
-                            offer our community and token holders early access 
-                            and investment opportunities. Our community 
-                            actively contributes to the growth and profitability of 
-                            these projects, creating a dynamic ecosystem of 
-                            innovation and shared success.
-                        </p>
-                    </div>
-                </div>
+                        {showText && (
+                            <p className='text-xs mt-8 pl-7 satoshi-regular'>
+                                At Creon, we handpick cutting-edge AI projects and 
+                                offer our community and token holders early access 
+                                and investment opportunities. Our community 
+                                actively contributes to the growth and profitability of 
+                                these projects, creating a dynamic ecosystem of 
+                                innovation and shared success.
+                            </p>
+                        )}
+                            </div>
+                        </div>
                 <div className='flex flex-row py-10 border-t border-gray-900'>
                     <div>
                         <img className="h-16 w-16 bottom-2 relative" src="./images/transparency.png" alt="Logo" />
